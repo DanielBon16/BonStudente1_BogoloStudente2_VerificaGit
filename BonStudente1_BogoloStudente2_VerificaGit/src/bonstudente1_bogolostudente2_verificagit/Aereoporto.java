05ecaf040615b9482de96dc9abec335912695c7e
Bon Daniel <daniel.bon@isitgo.it>
2022-11-30 08:47:46 +0100

    Aereoporto 2.0

--- BonStudente1_BogoloStudente2_VerificaGit/src/bonstudente1_bogolostudente2_verificagit/Aereoporto.java
+++ BonStudente1_BogoloStudente2_VerificaGit/src/bonstudente1_bogolostudente2_verificagit/Aereoporto.java
@@ -8,5 +8,44 @@ public class Aereoporto {
 
     public Aereoporto() {
     }
-     
+
+    public Aereoporto(String città, int superficie, int[] aerei, String[] piloti) {
+        this.città = città;
+        this.superficie = superficie;
+        this.aerei = aerei;
+        this.piloti = piloti;
+    }
+
+    public String getCittà() {
+        return città;
+    }
+
+    public void setCittà(String città) {
+        this.città = città;
+    }
+
+    public int getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(int superficie) {
+        this.superficie = superficie;
+    }
+
+    public int[] getAerei() {
+        return aerei;
+    }
+
+    public void setAerei(int[] aerei) {
+        this.aerei = aerei;
+    }
+
+    public String[] getPiloti() {
+        return piloti;
+    }
+
+    public void setPiloti(String[] piloti) {
+        this.piloti = piloti;
+    }
+    
 }
