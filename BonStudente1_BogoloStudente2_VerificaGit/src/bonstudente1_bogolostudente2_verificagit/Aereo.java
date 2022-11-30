c026fd1570f5b31d3aba9162b68a0a326e7e2b4b
DavideBognolo <davide.boglolo@isitgo.it>
2022-11-30 08:49:06 +0100

    Aereo

--- BonStudente1_BogoloStudente2_VerificaGit/src/bonstudente1_bogolostudente2_verificagit/Aereo.java
+++ BonStudente1_BogoloStudente2_VerificaGit/src/bonstudente1_bogolostudente2_verificagit/Aereo.java
@@ -4,10 +4,52 @@
  */
 package bonstudente1_bogolostudente2_verificagit;
 
-/**
- *
- * @author user
- */
+
 public class Aereo {
     
+   private String codice;
+   private int nPosti;
+   private String marca;
+   private String modello;
+
+    public Aereo(String codice, int nPosti, String marca, String modello) {
+        this.codice = codice;
+        this.nPosti = nPosti;
+        this.marca = marca;
+        this.modello = modello;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public int getnPosti() {
+        return nPosti;
+    }
+
+    public void setnPosti(int nPosti) {
+        this.nPosti = nPosti;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+   
+    //asdfghjkzxcvbnm   qwertyuiopè
 }
