d5a332a4e9717b10d9cebf9e206921b2e883b9f7
Bon Daniel <daniel.bon@isitgo.it>
2022-11-30 08:45:30 +0100

    Agiunto Aereoporto

---
+++ BonStudente1_BogoloStudente2_VerificaGit/src/bonstudente1_bogolostudente2_verificagit/Aereoporto.java
@@ -0,0 +1,12 @@
+package bonstudente1_bogolostudente2_verificagit;
+
+public class Aereoporto {
+    private String città;
+    private int superficie;
+    private int aerei[];
+    private String piloti[];
+
+    public Aereoporto() {
+    }
+     
+}
--- BonStudente1_BogoloStudente2_VerificaGit/src/bonstudente1_bogolostudente2_verificagit/BonStudente1_BogoloStudente2_VerificaGit.java
+++ BonStudente1_BogoloStudente2_VerificaGit/src/bonstudente1_bogolostudente2_verificagit/BonStudente1_BogoloStudente2_VerificaGit.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package bonstudente1_bogolostudente2_verificagit;
 
-/**
- *
- * @author user
- */
 public class BonStudente1_BogoloStudente2_VerificaGit {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
