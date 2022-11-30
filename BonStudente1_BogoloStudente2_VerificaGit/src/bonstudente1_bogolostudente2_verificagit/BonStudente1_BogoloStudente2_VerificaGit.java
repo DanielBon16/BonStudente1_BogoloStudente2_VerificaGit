1eef162b6c721276cb124713e9088a1c7c626e8d
DavideBognolo <davide.boglolo@isitgo.it>
2022-11-30 09:57:27 +0100

    Fine

--- BonStudente1_BogoloStudente2_VerificaGit/src/bonstudente1_bogolostudente2_verificagit/BonStudente1_BogoloStudente2_VerificaGit.java
+++ BonStudente1_BogoloStudente2_VerificaGit/src/bonstudente1_bogolostudente2_verificagit/BonStudente1_BogoloStudente2_VerificaGit.java
@@ -3,7 +3,22 @@ package bonstudente1_bogolostudente2_verificagit;
 public class BonStudente1_BogoloStudente2_VerificaGit {
 
     public static void main(String[] args) {
+    
+        Aereoporto aeroporto=new Aereoporto("Ronchi dei legionari",2830);
+        
+        String str= "L'aeroporto di "+aeroporto.getCittà()+" ha una superfice di "+aeroporto.getSuperficie()+" e gli aerei disponibili sono: \n";
+        
+        for (int i=0;i<3;i++)
+        {
+            str+="codice: "+aeroporto.getAerei()[i].getCodice()+", ";
+            str+="marca: "+aeroporto.getAerei()[i].getMarca()+", ";
+            str+="modello: "+aeroporto.getAerei()[i].getModello()+", ";
+            str+="numero posti:"+aeroporto.getAerei()[i].getnPosti()+"\n";
 
+
+        }
+        
+        System.out.println(str);
     }
     
 }
